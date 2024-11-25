0c9c4593ac96392211707b0548cfbc2f31d2cbec
Chien <chientrungha2004@gmail.com>
2024-11-26 00:37:49 +0700

    LAB03

---
+++ Lab03/AIMSProject/bin/dsai/aims/cart/Cart.java
@@ -0,0 +1,142 @@
+package hust.soict.dsai.aims.cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+public class Cart {
+    public static  final int MAX_NUMBERS_ORDERED = 20;  //So luong DVD toi da
+    private DigitalVideoDisc itemOrdered[] =
+            new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int quantity; //So luong DVD trong gio
+    private float totalCost;  //Tong gia tri DVD trong gio
+    public Cart() {
+        this.quantity = 0;
+        this.totalCost = 0;
+    }
+    //Them DVD vao gio hang
+    public void addDigitalVideoDisc (DigitalVideoDisc disc) {
+        if(quantity <= MAX_NUMBERS_ORDERED) {
+            itemOrdered[quantity] = disc;
+            quantity++;
+            totalCost += disc.getCost();
+        }
+        else {
+            System.out.println("The Cart is full. Can't add!");
+        }
+    }
+    public void addDigitalVideoDisc (DigitalVideoDisc[] dvdList) {
+        for(DigitalVideoDisc dvd : dvdList) {
+            if (quantity == MAX_NUMBERS_ORDERED) {
+                System.out.println("The Cart is full. Can't add!" + dvd.getTilte());
+                break;
+            }
+            this.itemOrdered[quantity] = dvd;
+            this.quantity++;
+            totalCost += dvd.getCost();
+        }
+    }
+    public void addDigitalVideoDisc(DigitalVideoDisc... dvdList) {
+        for(DigitalVideoDisc dvd : dvdList) {
+            if (quantity == MAX_NUMBERS_ORDERED) {
+                System.out.println("The Cart is full. Can't add!" + dvd.getTilte());
+                break;
+            }
+            this.itemOrdered[quantity] = dvd;
+            this.quantity++;
+            totalCost += dvd.getCost();
+        }
+    }
+    public void addDigitalVideoDisc (DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        if(quantity == MAX_NUMBERS_ORDERED) {
+            System.out.println("The Cart is full. Can't add!" + dvd1.getTilte() + "and" + dvd2.getTilte());
+        }
+        else {
+            if(quantity < MAX_NUMBERS_ORDERED) {
+                this.itemOrdered[quantity] = dvd1;
+                this.quantity++;
+                totalCost += dvd1.getCost();
+                System.out.println("Added " + dvd1.getTilte() + " successful!");
+            }
+            if(quantity < MAX_NUMBERS_ORDERED) {
+                this.itemOrdered[quantity] = dvd2;
+                this.quantity++;
+                totalCost += dvd2.getCost();
+                System.out.println("Added " + dvd2.getTilte() + " successful!");
+            }
+            else {
+                System.out.println("The Cart is full. Can't add!" + dvd2.getTilte());
+            }
+        }
+    }
+    //Xoa DVD ra khoi gio hang
+    public void removeDigitalVideoDisc (DigitalVideoDisc disc){
+        int index = -1;
+        for(int i = 0; i < quantity; i++) {
+            if(disc == itemOrdered[i]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1) {
+            System.out.println("Unavailble Disc");
+        }
+        else {
+            for(int i = index; i < quantity - 1; i++)
+            {
+                itemOrdered[i] = itemOrdered[i+1];
+            }
+            itemOrdered[quantity - 1] = null;
+            quantity--;
+            totalCost -= disc.getCost();
+            System.out.println("The Disc has been removed!");
+        }
+    }
+    //Tinh tong tien tat ca DVD
+    public float Total(){
+        return totalCost;
+    }
+    //dua ra danh sach cac DVD
+    public void DisplayCart() {
+        for(int i = 0; i < quantity; i++) {
+            System.out.println((i + 1) + ": " + itemOrdered[i].getTilte() + " with ID = " + itemOrdered[i].getId());
+        }
+    }
+    // In giỏ hàng
+    public void printCart() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+        float totalCost = 0;
+        for (int i = 0; i < quantity; i++) {
+            System.out.println((i + 1) + ". " + itemOrdered[i].toString());
+            totalCost += itemOrdered[i].getCost();
+        }
+        System.out.println("Total cost: " + totalCost + " $");
+        System.out.println("***************************************************");
+    }
+    // Tìm kiếm DVD theo id
+    public void searchById(int id) {
+        boolean found = false;
+        for (int i = 0; i < quantity; i++) {
+            DigitalVideoDisc disc = itemOrdered[i];
+            if (disc.getId() == id) {
+                System.out.println("Found: " + disc.toString());
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("No DVD found with ID: " + id);
+        }
+    }
+    // Tìm kiếm DVD theo tiêu đề
+    public void searchByTitle(String title) {
+        boolean found = false;
+        for (int i = 0; i < quantity; i++) {
+            DigitalVideoDisc disc = itemOrdered[i];
+            if (disc.isMatch(title)) {
+                System.out.println("Found: " + disc.toString());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No DVD found with title: " + title);
+        }
+    }
+}
---
+++ Lab03/OtherProject/hust/soict/dsai/Lab01/src/AddTwoMatrix.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class AddTwoMatrix {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int row = sc.nextInt();
+        System.out.print("Enter number of collumns: ");
+        int col = sc.nextInt();
+
+        //Khai báo ma trận số thực
+        double[][] matA = new double[row][col]; //Ma trận A
+        double[][] matB = new double[row][col]; //Ma trận B
+        double[][] matAns = new double[row][col]; //Ma trận kết quả
+
+        //Nhập các phần tử trong các ma trận
+        System.out.println("\nEnter Element of Matrix A: ");
+        for (int i=0;i<row;i++){
+            for (int j=0;j<col;j++){
+                matA[i][j] = sc.nextDouble();
+            }
+        }
+        System.out.println("\nEnter Element of Matrix B: ");
+        for (int i=0;i<row;i++){
+            for (int j=0;j<col;j++){
+                matB[i][j] = sc.nextDouble();
+            }
+        }
+
+        //Tính matA + matB
+        for (int i=0;i<row;i++){
+            for (int j=0;j<col;j++){
+                matAns[i][j] = matA[i][j] + matB[i][j];
+            }
+        }
+
+        //In kết quả
+        System.out.println("\n________________________________");
+        System.out.println("Result: ");
+        for (int i=0;i<row;i++){
+            for (int j=0;j<col;j++){
+                System.out.print(matAns[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
